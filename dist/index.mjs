4e78314113617faa1ac4b67c610320878088f05d
alexpohl <apohlbermudez@gmail.com>
2023-09-25 11:08:30 +0200

    cap a dins

---
+++ dist/index.mjs
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Enter your first number: ",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Enter your second number: ",
+    },
+    {
+        type: "list",
+        name: "operators",
+        choices: ['+', '-', '*', '/'],
+        message: "Select operator: ",
+    }
+]);
+console.log(answers);
+let { numberOne, numberTwo, operators } = answers;
+if (numberOne && numberTwo && operators) {
+    let result = 0;
+    if (operators === '+') {
+        result = numberOne + numberTwo;
+    }
+    else if (operators === '-') {
+        result = numberOne - numberTwo;
+    }
+    else if (operators === '*') {
+        result = numberOne * numberTwo;
+    }
+    else if (operators === '/') {
+        result = numberOne / numberTwo;
+    }
+    console.log(`Your result is ${result}`);
+}
+else {
+    console.log('Enter valid input.');
+}
